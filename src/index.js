6de4055d8c6c4a6d05a1a7c1ad91ed57308fb356
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-05-22 19:55:56 +0300

    update basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GlobalStyles } from 'GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/PsychologistsServices">
+    <BrowserRouter basename="/">
       <GlobalStyles />
       <App />
     </BrowserRouter>
