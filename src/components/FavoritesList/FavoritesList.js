c80b676492b3d59553aae4596acc0c9d0b437ae9
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-05-30 00:19:12 +0300

    favorite filter and styled

--- src/components/FavoritesList/FavoritesList.js
+++ src/components/FavoritesList/FavoritesList.js
@@ -1,52 +1,68 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { initializeFavorites } from '../../redux/psychologists/favoriteReducer';
 import { PsychoItem } from 'components/PsychoItem/PsychoItem';
 import Filter from 'components/Filter/Filter';
-
-const sortFavorites = (favorites, sortBy) => {
-  switch (sortBy) {
-    case 'name_asc':
-      return [...favorites].sort((a, b) => a.name.localeCompare(b.name));
-    case 'name_desc':
-      return [...favorites].sort((a, b) => b.name.localeCompare(a.name));
-    case 'price_high_low':
-      return [...favorites].sort((a, b) => b.price_per_hour - a.price_per_hour);
-    case 'price_low_high':
-      return [...favorites].sort((a, b) => a.price_per_hour - b.price_per_hour);
-    case 'popular':
-      return [...favorites].sort((a, b) => b.rating - a.rating);
-    case 'not_popular':
-      return [...favorites].sort((a, b) => a.rating - b.rating);
-    case 'all':
-    default:
-      return favorites;
-  }
-};
+import { FavoriteContainer } from './FavoritesList.styled';
+import { setSortBy } from '../../redux/psychologists/psychoReducer';
+import { PsychoStyledList } from 'components/PsychoList/PsychoList.styled';
 
 export const FavoritesList = () => {
   const dispatch = useDispatch();
+
   const favorites = useSelector(state => state.favorites.items);
   const sortBy = useSelector(state => state.psychologists.sortBy);
+  const [isFilterOpen, setFilterOpen] = useState(false);
 
   useEffect(() => {
     dispatch(initializeFavorites());
   }, [dispatch]);
 
-  const sortedFavorites = sortFavorites(favorites, sortBy);
-
   if (favorites.length === 0) {
     return <div>No favorite psychologists.</div>;
   }
 
+  const toggleFilter = () => {
+    setFilterOpen(!isFilterOpen);
+  };
+
+  const handleSelect = sortBy => {
+    dispatch(setSortBy(sortBy));
+    setFilterOpen(false);
+  };
+
+  const sortedPsychologists = [...favorites].sort((a, b) => {
+    switch (sortBy) {
+      case 'name_asc':
+        return a.name.localeCompare(b.name);
+      case 'name_desc':
+        return b.name.localeCompare(a.name);
+      case 'price_high_low':
+        return b.price_per_hour - a.price_per_hour;
+      case 'price_low_high':
+        return a.price_per_hour - b.price_per_hour;
+      case 'popular':
+        return b.rating - a.rating;
+      case 'not_popular':
+        return a.rating - b.rating;
+      case 'all':
+      default:
+        return 0;
+    }
+  });
+
   return (
-    <div className="container">
-      <Filter />
-      <ul>
-        {sortedFavorites.map((psychologist, index) => (
+    <FavoriteContainer className="container">
+      <Filter
+        isOpen={isFilterOpen}
+        onToggle={toggleFilter}
+        onSelect={handleSelect}
+      />
+      <PsychoStyledList>
+        {sortedPsychologists.map((psychologist, index) => (
           <PsychoItem key={index} psychologist={psychologist} index={index} />
         ))}
-      </ul>
-    </div>
+      </PsychoStyledList>
+    </FavoriteContainer>
   );
 };
---
+++ src/components/FavoritesList/FavoritesList.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const FavoriteContainer = styled.div`
+  padding-top: 64px;
+  padding-bottom: 64px;
+`;
